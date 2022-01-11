880c86f8c651a68e65e3574529d73bbb53ceb225
Yegane Ahmadnejad <yegane.ahmadnejad@snapp.cab>
2022-01-11 08:58:17 +0330

    add network block

--- fanout.go
+++ fanout.go
@@ -2,12 +2,13 @@ package retry
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/plugin/pkg/parse"
 	"github.com/coredns/coredns/plugin/pkg/transport"
+	"github.com/pkg/errors"
 	"github.com/yeganeahmadnejad/fanout"
-
 )
 
 func initFanout(c *caddy.Controller) (*fanout.Fanout, error) {
@@ -28,18 +29,48 @@ func initFanout(c *caddy.Controller) (*fanout.Fanout, error) {
 	if err != nil {
 		return f, err
 	}
-
+	//set default value
+	net := "udp"
 	for c.NextBlock() {
-		return f, fmt.Errorf("additional parameters not allowed")
+		if strings.ToLower(c.Val()) == "network" {
+			// shift cursor past network
+			if !c.Next() {
+				return f, c.ArgErr()
+			}
+			net, err = parseProtocol(c)
+			if err != nil {
+				return f, err
+			}
+
+		} else {
+			return f, fmt.Errorf("additional parameters not allowed")
+
+		}
+
 	}
+
+	// for c.NextBlock() {
+	// 	return f, fmt.Errorf("additional parameters not allowed")
+	// }
 	for _, host := range toHosts {
 		trans, h := parse.Transport(host)
 		if trans != transport.DNS {
 			return f, fmt.Errorf("only dns transport allowed")
 		}
-		client := fanout.NewClient(h, "udp")
+		client := fanout.NewClient(h, net)
 		f.AddClient(client)
 
 	}
 	return f, nil
+}
+
+func parseProtocol(c *caddy.Controller) (string, error) {
+	if !c.NextArg() {
+		return "", c.ArgErr()
+	}
+	net := strings.ToLower(c.Val())
+	if net != "tcp" && net != "udp" && net != "tcptlc" {
+		return "", errors.New("unknown network protocol")
+	}
+	return net, nil
 }
